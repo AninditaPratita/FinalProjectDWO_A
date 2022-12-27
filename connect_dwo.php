e11e102f56d91e8a5d86fb1c5e90a0bb9f981240
AninditaPratita <99853992+AninditaPratita@users.noreply.github.com>
2022-12-27 19:05:39 +0700

    Delete connect_dwo.php

--- connect_dwo.php
+++
@@ -1,9 +0,0 @@
-<?php
-//konfigurasi ke database
-$host		="localhost";
-$username	="root";
-$password	="";
-$database	="dwo_multidimensi5";
-//membuat koneksi
-$conn		= mysqli_connect($host, $username, $password, $database);
-?>
